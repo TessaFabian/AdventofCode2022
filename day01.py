21d64a0e3b8ea8beaae906b7f59e15da90961225
TessaFabian <tessa.fabian@web.de>
2022-12-08 08:56:26 +0100

    Solved day 1

--- day01.py
+++ day01.py
@@ -1,28 +1,25 @@
 def getMostCalories(file):
+  #calculates solution for part 1
   caloriesArray = []
   sumCaloriesArray = []
   sumCalories = 0
+  name = ""
   with open(file, "r") as file:
+    name = file.name
     for line in file:
       if line != "\n":
         sumCalories += int(line)
       else:
         sumCaloriesArray.append(sumCalories)
         sumCalories = 0
-  sumCaloriesArray.sort()
-  print(sumCaloriesArray[len(sumCaloriesArray)-1])
+  print(name)
+  print("Solution part 1:")
+  print(max(sumCaloriesArray))
+  sumCaloriesArray.sort(reverse=True)
+  print("Solution part 2:")
+  print(sumCaloriesArray[0]+sumCaloriesArray[1]+sumCaloriesArray[2])
 
-
-
-def buildCaloriesArray(file):
-  caloriesArray = []
-  with open(file, "r") as file:
-    for line in file:
-      if line == "\n":
-        caloriesArray.append("n")
-      else:
-        caloriesArray.append(int(line))
-  return caloriesArray
+ 
 
 getMostCalories("testday01.out")
 getMostCalories("day01.out")
