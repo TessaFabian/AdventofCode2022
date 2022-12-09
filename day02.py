780bd5656f375cae9368713ee92020403fae290a
TessaFabian <tessa.fabian@web.de>
2022-12-09 23:17:23 +0100

    Day 2, part 2 solved

--- day02.py
+++ day02.py
@@ -14,8 +14,42 @@ def calcTotalScore(strategyFile):
                 sumScore += valuesPlayerShapes[line[2]]
     print("total score: ", sumScore)
 
-print("-------- Example guide  ------------")
+def calcUltraScore(strategyFile):
+    roundEndValues = {"X": 0, "Y": 3, "Z": 6}
+    strategyWinMap = {"A": "Y", "B": "Z", "C": "X"}
+    strategyDrawMap = {"A": "X", "B": "Y", "C": "Z"}
+    strategyLostMap = {"A": "Z", "B": "X", "C": "Y"}
+    valuesPlayerShapes = {"X": 1, "Y": 2, "Z": 3}
+
+    sumScore = 0
+
+    with open(strategyFile, "r") as file:
+        for line in file:
+            if roundEndValues[line[2]] == 0:
+                #lost
+                playerWeapon = strategyLostMap[line[0]]
+                sumScore += valuesPlayerShapes[playerWeapon]
+            elif roundEndValues[line[2]] == 3:
+                #draw
+                playerWeapon = strategyDrawMap[line[0]]
+                sumScore += 3 + valuesPlayerShapes[playerWeapon]
+            elif roundEndValues[line[2]] == 6:
+                #win
+                playerWeapon = strategyWinMap[line[0]]
+                sumScore += 6 + valuesPlayerShapes[playerWeapon]
+    print("total score: ", sumScore)
+
+
+print("-------- Example guide, part 1  ------------")
 calcTotalScore("testday02.out")
 
-print("-------- Official guide  ------------")
+print("-------- Example guide, part 2  ------------")
+calcUltraScore("testday02.out")
+
+print("-------- Official guide, part 1 ------------")
 calcTotalScore("day02.out")
+
+print("-------- Official guide, part 2 ------------")
+calcUltraScore("day02.out")
+
+
